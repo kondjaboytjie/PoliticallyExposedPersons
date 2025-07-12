c991cc9a544c54acf47544df0f3b9191cb6b1c99
kondjaboytjie <kondjaboykie@gmail.com>
2025-07-12 18:43:15 +0200

    fixed sessions

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -6,16 +6,16 @@ export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const token = localStorage.getItem("token");
+    const storedUser = sessionStorage.getItem("user");
+    const token = sessionStorage.getItem("token");
 
     if (storedUser && storedUser !== "undefined" && token) {
       try {
         setUser(JSON.parse(storedUser));
       } catch (err) {
-        console.error("Invalid JSON in localStorage for user:", err);
+        console.error("Invalid JSON in sessionStorage for user:", err);
         setUser(null);
-        localStorage.removeItem("user");
+        sessionStorage.removeItem("user");
       }
     } else {
       setUser(null);
